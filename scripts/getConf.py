0e4ca788219d7c92c6b68a0c2d85ed173d4bef93
SSC-I KLOWED <cloudbeheer@dji.minjus.nl>
2016-07-07 16:26:10 +0200

    new_cluster really starting to be something. now starting with deploying the template

--- scripts/getConf.py
+++ scripts/getConf.py
@@ -4,6 +4,7 @@ import os
 import sys
 import argparse
 import re
+from StringIO import StringIO
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-f', '--filename', dest='filename', help='regexp for files to include.', default='.*')
@@ -11,8 +12,6 @@ parser.add_argument('-c', '--chapter', dest='chapter', help='chapter where the k
 parser.add_argument('-k', '--keyname', dest='key', help='chapter where the key should be')
 parser.add_argument('rootdirs', nargs='+', help='folders to scan the ceph.conf files recide.')
 args = parser.parse_args()
-
-import ConfigParser
 
 fsids = set()
 filetester = re.compile(args.filename)
@@ -24,8 +23,9 @@ for rd in args.rootdirs:
             if not filetester.search(f):
                 continue
             try:
+                data = StringIO('\n'.join(line.strip() for line in open(os.path.join(dir,f))))
                 config = ConfigParser.RawConfigParser()
-                config.read(os.path.join(dir, f))
+                config.readfp(data)
                 fsids.add(config.get(args.chapter,args.key))
             except:
                 pass
