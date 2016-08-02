6a27af17f6d2d35f477c59a1600719b8ed306860
SSC-I KLOWED <cloudbeheer@dji.minjus.nl>
2016-08-02 18:41:04 +0200

    Added some stuff to proxy_settings.sh (ftp_proxy and no_proxy)

--- roles/common/templates/proxy_settings.sh
+++ roles/common/templates/proxy_settings.sh
@@ -1,3 +1,6 @@
 # KLOWED Proxy exports
 export http_proxy={{ proxy_server }}
 export https_proxy="${http_proxy}"
+export ftp_proxy="${http_proxy}"
+export no_proxy="localhost,127.0.0.1,$(hostname),.$(hostname -d)"
+
